e5f38ef51c4c9e5ddae27616cac4cbc34ea33111
jmasSN <juliette.massy@sorbonne-nouvelle.fr>
2022-11-16 11:27:41 +0100

    djshdui

--- script/traitement_url_base.sh
+++ script/traitement_url_base.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
 #===============================================================================
-# VOUS DEVEZ MODIFIER CE BLOC DE COMMENTAIRES.
-# Ici, on décrit le comportement du programme.
-# Indiquez, entre autres, comment on lance le programme et quels sont
+# Ce script est notre premier programme pour traiter les URLs. Il permet de créer un tableau HTML en sortie avec chaque URL, le code de réponse correspondant .
+# Il s'utilise dans le dossier avec le fichier à traiter <URL.txt> (avec un URL/ligne) avec la ligne de commande :
+# bash traitement_url_base.sh <URL.txt>  <tableauURL.html> 
+
+#<tableauURL.html>  est le fichier de sortie du tableau HTML (ici il n'existe pas avant le lancement du script).
+#===============================================================================
+
+# On vérifie que l'on a 2 arguments :
 if [ $# -ne 2 ]
 then
 	echo " ce programme demande deux arguments"
 	exit
 fi
-# les paramètres.
-# La forme est indicative, sentez-vous libres d'en changer !
-# Notamment pour quelque chose de plus léger, il n'y a pas de norme en bash.
-#===============================================================================
 
+# On vérifie que le fichier d'URLs existe :
+if [ -f $1 ]
+	then
+		echo "Fichier OK"
+	else
+		echo "Ceci n'est pas un fichier valide."
+		exit
+fi
+
+# On attribue les fichiers en argument à des variables :
 fichier_urls=$1 # le fichier d'URL en entrée
 fichier_tableau=$2 # le fichier HTML en sortie
-# !!!!!!
-# ici on doit vérifier que nos deux paramètres existent, sinon on ferme!
-# !!!!!!
 
-# modifier la ligne suivante pour créer effectivement du HTML
-
-
-
-   
+# On crée l'architecture HTML du tableau de sortie dans le fichier donné en second argument :
 echo "<html>
         <head>
                 <meta charset ="utf-8"/>
@@ -36,15 +40,25 @@ echo "<html>
                 <th> ligne </th>
                 <th>code</th>
                 <th>url</th> 
-                </tr>" >$fichier_tableau
-                lineno=1;
-                while read -r line;
-do
-        URL=$line
-        CODEHTTP=$(curl -I -s $line | head -n1)	
-	echo "<tr><td>$lineno</td><td>$CODEHTTP</td><td>$URL</td></tr>" >> $fichier_tableau
-	lineno=$((lineno+1));
+                </tr>" > $fichier_tableau
                 
-       done < $fichier_urls               
+ # On crée un compteur pour les URLs/le nombre de lignes :               
+lineno=1;
 
+# On lit chaque ligne :
+while read -r line;
+do
+        # On récupère l'URL (=la ligne)
+        URL=$line
+        # On récupère le code de réponse :
+        CODEHTTP=$(curl -I -s $line | head -n1)
+        # On ajoute une ligne avec le code de réponse et l'URL au tableau HTML :
+        # Les doubles chevrons permettent de ne pas écraser le fichier de sortie.
+	    echo "<tr><td>$lineno</td><td>$CODEHTTP</td><td>$URL</td></tr>" >> $fichier_tableau
+	    # On incrément le compteur :
+	    lineno=$((lineno+1));
+# On clot la boucle et on indique le fichier à parcourir :
+done < $fichier_urls           
+    
+# On clot le tableau HTML :
         echo "</table></body></html>" >> $fichier_tableau
